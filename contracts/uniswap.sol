52972a7ff97ed809b236a2cf8e7478c9c4179692
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2023-05-27 11:54:43 +0100

    uniswap

--- contracts/uniswap.sol
+++ contracts/uniswap.sol
@@ -1,4 +1,99 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+
+contract UniswapV2Swap is IUniswapV2Swap {
+
+    IUniswapV2Factory public factory;
+
+    constructor(address factory_) public {
+        factory = IUniswapV2Factory(factory_);
+    }
+
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) public returns (uint256 amountOut) {
+        return factory.swapExactTokensForTokens(
+            amountIn,
+            amountOutMin,
+            path,
+            to,
+            deadline
+        );
+    }
+
+    function swapTokensForExactTokens(
+        uint256 amountOut,
+        uint256 amountInMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) public returns (uint256 amountIn) {
+        return factory.swapTokensForExactTokens(
+            amountOut,
+            amountInMin,
+            path,
+            to,
+            deadline
+        );
+    }
+
+    // Add a function to add liquidity to a pool.
+    function addLiquidity(
+        uint256 amountTokenA,
+        uint256 amountTokenB,
+        uint256 minLiquidity,
+        address to,
+        uint256 deadline
+    ) public returns (uint liquidity) {
+        // Calculate the amount of liquidity to add.
+        uint amount = amountTokenA * amountTokenB / (factory.getReserves(tokenA, tokenB)[0] * factory.getReserves(tokenB, tokenA)[1]);
+
+        // Add liquidity to the pool.
+        return factory.addLiquidity(
+            amountTokenA,
+            amountTokenB,
+            minLiquidity,
+            to,
+            deadline
+        );
+    }
+
+    // Add a function to remove liquidity from a pool.
+    function removeLiquidity(
+        uint256 amountOutMin,
+        uint256 amountTokenAMin,
+        uint256 amountTokenBMin,
+        address to,
+        uint256 deadline
+    ) public returns (uint amountTokenA, uint amountTokenB) {
+        return factory.removeLiquidity(
+            amountOutMin,
+            amountTokenAMin,
+            amountTokenBMin,
+            to,
+            deadline
+        );
+    }
+
+    // Add a function to get the reserves of a pool.
+    function getReserves(
+        address tokenA,
+        address tokenB,
+        uint256 deadline
+    ) public returns (uint reserveA, uint reserveB) {
+        return factory.getReserves(
+            tokenA,
+            tokenB,
+            deadline
+        );
+    }
+
+}
